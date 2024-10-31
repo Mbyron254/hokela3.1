89b4093ddc587b0a64b9a256f40b25ca20e61dd5
Mbyron254 <byron@hokela.co.ke>
2024-11-01 02:57:52 +0300

    Add  Sign Up Components

--- src/auth/view/main/jwt-sign-up-view.tsx
+++ src/auth/view/main/jwt-sign-up-view.tsx
@@ -1,16 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 'use client';
 
 import { z as zod } from 'zod';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import MuiPhoneNumber from 'mui-phone-number';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Alert from '@mui/material/Alert';
-import IconButton from '@mui/material/IconButton';
 import LoadingButton from '@mui/lab/LoadingButton';
-import InputAdornment from '@mui/material/InputAdornment';
 
 import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
@@ -18,10 +19,8 @@ import { RouterLink } from 'src/routes/components';
 
 import { useBoolean } from 'src/hooks/use-boolean';
 
-import { Iconify } from 'src/components/iconify';
 import { Form, Field } from 'src/components/hook-form';
 
-import { signUp } from '../../context/main';
 import { useAuthContext } from '../../hooks';
 import { FormHead } from '../../components/form-head';
 import { SignUpTerms } from '../../components/sign-up-terms';
@@ -31,8 +30,8 @@ import { SignUpTerms } from '../../components/sign-up-terms';
 export type SignUpSchemaType = zod.infer<typeof SignUpSchema>;
 
 export const SignUpSchema = zod.object({
-  firstName: zod.string().min(1, { message: 'First name is required!' }),
-  lastName: zod.string().min(1, { message: 'Last name is required!' }),
+  officialName: zod.string().min(1, { message: 'First name is required!' }),
+  phoneNumber: zod.string().min(1, { message: 'Last name is required!' }),
   email: zod
     .string()
     .min(1, { message: 'Email is required!' })
@@ -53,12 +52,14 @@ export function JwtSignUpView() {
   const password = useBoolean();
 
   const [errorMsg, setErrorMsg] = useState('');
+  const [value, setValue] = useState();
 
   const defaultValues = {
-    firstName: 'Hello',
-    lastName: 'Friend',
-    email: 'hello@gmail.com',
-    password: '@demo1',
+    officialName: '',
+    phoneNumber: '',
+
+    email: '',
+    // password: '@demo1',
   };
 
   const methods = useForm<SignUpSchemaType>({
@@ -73,12 +74,12 @@ export function JwtSignUpView() {
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      await signUp({
-        email: data.email,
-        password: data.password,
-        firstName: data.firstName,
-        lastName: data.lastName,
-      });
+      // await signUp({
+      //   email: data.email,
+      //   // password: data.password,
+      //   officialName: data.officialName,
+      //   phoneNumber: data.phoneNumber,
+      // });
       await checkUserSession?.();
 
       router.refresh();
@@ -91,29 +92,16 @@ export function JwtSignUpView() {
   const renderForm = (
     <Box gap={3} display="flex" flexDirection="column">
       <Box display="flex" gap={{ xs: 3, sm: 2 }} flexDirection={{ xs: 'column', sm: 'row' }}>
-        <Field.Text name="firstName" label="First name" InputLabelProps={{ shrink: true }} />
-        <Field.Text name="lastName" label="Last name" InputLabelProps={{ shrink: true }} />
+        <Field.Text name="officialName" label="Official Name" InputLabelProps={{ shrink: true }} />
       </Box>
-
       <Field.Text name="email" label="Email address" InputLabelProps={{ shrink: true }} />
-
-      <Field.Text
-        name="password"
-        label="Password"
-        placeholder="6+ characters"
-        type={password.value ? 'text' : 'password'}
-        InputLabelProps={{ shrink: true }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton onClick={password.onToggle} edge="end">
-                <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
+      {/* <Field.Text name="phoneNumber" label="Phone Number" InputLabelProps={{ shrink: true }} /> */}
+      <MuiPhoneNumber
+        name="phoneNumber"
+        defaultCountry="ke"
+        // @ts-ignore
+        onChange={(newValue) => setValue(newValue)}
       />
-
       <LoadingButton
         fullWidth
         color="inherit"
