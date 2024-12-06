ffec1e86d1d405f7c9e98f3ef34c55ff4091ee2b
kmaurice012 <kingmaurice012@gmail.com>
2024-12-06 10:00:40 +0300

    Fetch Sales Survey

---
+++ src/sections/campaigns/details/sales-report.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import Typography from '@mui/material/Typography';
+
+import { GQLMutation } from 'src/lib/client';
+import { SALES_GIVEAWAY_SURVEY } from 'src/lib/mutations/sales-giveaway.mutation';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  campaignRunId?: string;
+};
+
+export default function SalesReport({ campaignRunId }: Props) {
+  const { action: getSurvey, data: survey } = GQLMutation({
+    mutation: SALES_GIVEAWAY_SURVEY,
+    resolver: 'salesGiveawaySurvey',
+    toastmsg: false,
+  });
+
+  const loadSurvey = () => {
+    if (campaignRunId) {
+      getSurvey({ variables: { input: { campaignRunId } } });
+    }
+  };
+  useEffect(() => {
+    if (campaignRunId) {
+      loadSurvey();
+    } else {
+      throw new Error('Campaign Run ID is required');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loadSurvey]);
+
+  console.log('SURVEY', survey);
+  return (
+    <>
+      <Typography variant="h6" sx={{ mb: 3, color: 'primary.main', fontWeight: 700 }}>
+        Product Sales Dashboard
+      </Typography>
+
+      {/* Copy the rest of the JSX from TabPanel value="1" */}
+    </>
+  );
+}
