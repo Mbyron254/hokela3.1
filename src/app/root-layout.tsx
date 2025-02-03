23a47813edb1865bd3488e73113214a7b1db9e0c
Mbyron254 <byron@hokela.co.ke>
2025-02-03 12:06:21 +0300

    Add Favicon

--- src/app/root-layout.tsx
+++ src/app/root-layout.tsx
@@ -7,12 +7,25 @@ export const viewport = {
   themeColor: primary.main,
 };
 
+// export const metadata = {
+//   icons: [
+//     {
+//       rel: 'icon',
+//       url: `src/assets/favicon.ico`,
+//     },
+//   ],
+// };
 export const metadata = {
+  title: 'Hokela Interactive',
+  description:
+    'Manage your business transaction and document ans human resource all under a single app',
+  keywords: 'sales,merchandising,document-management,resource-tracking',
+  manifest: '/manifest.json',
   icons: [
-    {
-      rel: 'icon',
-      url: `src/assets/favicon.ico`,
-    },
+    { rel: 'icon', url: '/favicon/favicon.ico' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon/favicon-32x32.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon/apple-touch-icon.png' },
   ],
 };
 
