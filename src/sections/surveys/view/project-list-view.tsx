f8e278c6f7a573fff906847566f399109f299c81
Mbyron254 <byron@hokela.co.ke>
2025-02-24 11:42:24 +0300

    Add Surveys Section

---
+++ src/sections/surveys/view/project-list-view.tsx
@@ -0,0 +1,528 @@
+'use client';
+
+import type { IOrderItem, IOrderTableFilters } from 'src/types/project';
+
+import { useState, useCallback } from 'react';
+
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+
+import { useBoolean } from 'src/hooks/use-boolean';
+import { useSetState } from 'src/hooks/use-set-state';
+
+import { fIsAfter, fIsBetween } from 'src/utils/format-time';
+
+import { varAlpha } from 'src/theme/styles';
+import { DashboardContent } from 'src/layouts/dashboard';
+import { _orders, STATUS_OPTION, ALL_CLIENTS, ALL_MANAGERS } from 'src/_mock';
+
+import { Label } from 'src/components/label';
+import { toast } from 'src/components/snackbar';
+import { Iconify } from 'src/components/iconify';
+import { Scrollbar } from 'src/components/scrollbar';
+import { ConfirmDialog } from 'src/components/custom-dialog';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
+import {
+  useTable,
+  emptyRows,
+  rowInPage,
+  TableNoData,
+  getComparator,
+  TableEmptyRows,
+  TableHeadCustom,
+  TableSelectedAction,
+  TablePaginationCustom,
+} from 'src/components/table';
+
+import {
+  Autocomplete,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Tab,
+  Box,
+  Tabs,
+  Card,
+  Table,
+  Button,
+  Tooltip,
+  TableBody,
+  IconButton,
+} from '@mui/material';
+import { MobileDatePicker } from '@mui/x-date-pickers';
+import { IDatePickerControl } from 'src/types/common';
+import dayjs from 'dayjs';
+
+import { OrderTableRow } from '../project-table-row';
+import { OrderTableFiltersResult } from '../project-table-filters-result';
+
+// ----------------------------------------------------------------------
+
+const STATUS_OPTIONS = [{ value: 'all', label: 'All' }, ...STATUS_OPTION];
+
+const TABLE_HEAD = [
+  { id: 'orderNumber', label: 'Order', width: 88 },
+  { id: 'name', label: 'Customer' },
+  { id: 'createdAt', label: 'Date', width: 140 },
+  {
+    id: 'totalQuantity',
+    label: 'Items',
+    width: 120,
+    align: 'center',
+  },
+  { id: 'totalAmount', label: 'Price', width: 140 },
+  { id: 'status', label: 'Status', width: 110 },
+  { id: '', width: 88 },
+];
+
+// ----------------------------------------------------------------------
+
+export function ProjectListView() {
+  const table = useTable({ defaultOrderBy: 'orderNumber' });
+
+  const router = useRouter();
+
+  const confirm = useBoolean();
+
+  const dialog = useBoolean();
+
+  const isEdit = useBoolean();
+
+  const [tableData, setTableData] = useState<IOrderItem[]>(_orders);
+
+  const [value, setValue] = useState<IDatePickerControl>(dayjs(new Date()));
+
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+
+  const filters = useSetState<IOrderTableFilters>({
+    name: '',
+    status: 'all',
+    startDate: null,
+    endDate: null,
+  });
+
+  const dateError = fIsAfter(filters.state.startDate, filters.state.endDate);
+
+  const dataFiltered = applyFilter({
+    inputData: tableData,
+    comparator: getComparator(table.order, table.orderBy),
+    filters: filters.state,
+    dateError,
+  });
+
+  const dataInPage = rowInPage(dataFiltered, table.page, table.rowsPerPage);
+
+  const canReset =
+    !!filters.state.name ||
+    filters.state.status !== 'all' ||
+    (!!filters.state.startDate && !!filters.state.endDate);
+
+  const notFound = (!dataFiltered.length && canReset) || !dataFiltered.length;
+
+  const handleNewRow = useCallback(() => {
+    // isEdit.onFalse();
+    dialog.onTrue();
+
+    // router.push(paths.dashboard.product.edit(id));
+  }, []);
+  const handleEditRow = (id: string) => {
+    console.log(id, 'IDENTIFICATION');
+
+    isEdit.onTrue();
+    dialog.onTrue();
+  };
+  const handleDeleteRow = useCallback(
+    (id: string) => {
+      const deleteRow = tableData.filter((row) => row.id !== id);
+
+      toast.success('Delete success!');
+
+      setTableData(deleteRow);
+
+      table.onUpdatePageDeleteRow(dataInPage.length);
+    },
+    [dataInPage.length, table, tableData]
+  );
+
+  const handleDeleteRows = useCallback(() => {
+    const deleteRows = tableData.filter((row) => !table.selected.includes(row.id));
+
+    toast.success('Delete success!');
+
+    setTableData(deleteRows);
+
+    table.onUpdatePageDeleteRows({
+      totalRowsInPage: dataInPage.length,
+      totalRowsFiltered: dataFiltered.length,
+    });
+  }, [dataFiltered.length, dataInPage.length, table, tableData]);
+
+  const handleViewRow = useCallback((id: string) => {
+    router.push(paths.v2.marketing.projects.details(id));
+  }, []);
+
+  const handleFilterStatus = useCallback(
+    (event: React.SyntheticEvent, newValue: string) => {
+      table.onResetPage();
+      filters.setState({ status: newValue });
+    },
+    [filters, table]
+  );
+  const handleSubmit = () => {
+    // if (isEdit.value && editingClient) {
+    //   // Update existing client
+    //   const updatedData = tableData.map((item) =>
+    //     item.id === editingClient.id
+    //       ? {
+    //           ...item,
+    //           name: formData.name,
+    //           clientType: formData.clientType,
+    //         }
+    //       : item
+    //   );
+    //   setTableData(updatedData);
+    //   toast.success('Client updated successfully!');
+    // } else {
+    //   // Create new client
+    //   const newClient = {
+    //     id: String(tableData.length + 1),
+    //     name: formData.name,
+    //     clientType: formData.clientType,
+    //     noOfProjects: 0,
+    //     createdAt: new Date().toISOString(),
+    //   };
+    //   // setTableData([...tableData, newClient]);
+    //   toast.success('Client created successfully!');
+    // }
+    handleDialogClose();
+  };
+  const handleDialogClose = () => {
+    setFormData({ name: '' });
+    // setEditingClient(null);
+    dialog.onFalse();
+  };
+
+  // Add these new handlers
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <DashboardContent>
+        <CustomBreadcrumbs
+          heading="List"
+          links={[
+            { name: 'Dashboard', href: paths.v2.marketing.root },
+            { name: 'Surveys', href: paths.v2.marketing.projects.list },
+            { name: 'List' },
+          ]}
+          action={
+            <Button
+              // component={RouterLink}
+              // href={paths.v2.marketing.clients.new}
+              onClick={() => {
+                handleNewRow();
+                isEdit.onFalse();
+              }}
+              variant="contained"
+              startIcon={<Iconify icon="mingcute:add-line" />}
+            >
+              New Surveys
+            </Button>
+          }
+          sx={{ mb: { xs: 3, md: 5 } }}
+        />
+
+        <Card>
+          <Tabs
+            value={filters.state.status}
+            onChange={handleFilterStatus}
+            sx={{
+              px: 2.5,
+              boxShadow: (theme) =>
+                `inset 0 -2px 0 0 ${varAlpha(theme.vars.palette.grey['500Channel'], 0.08)}`,
+            }}
+          >
+            {STATUS_OPTIONS.map((tab) => (
+              <Tab
+                key={tab.value}
+                iconPosition="end"
+                value={tab.value}
+                label={tab.label}
+                icon={
+                  <Label
+                    variant={
+                      ((tab.value === 'all' || tab.value === filters.state.status) && 'filled') ||
+                      'soft'
+                    }
+                    color={
+                      (tab.value === 'active' && 'success') ||
+                      (tab.value === 'suspended' && 'warning') ||
+                      'default'
+                    }
+                  >
+                    {['completed', 'pending', 'cancelled', 'refunded'].includes(tab.value)
+                      ? tableData.filter((user) => user.status === tab.value).length
+                      : tableData.length}
+                  </Label>
+                }
+              />
+            ))}
+          </Tabs>
+
+          {/* <ProjectTableToolbar
+            filters={filters}
+            onResetPage={table.onResetPage}
+            dateError={dateError}
+          /> */}
+
+          {canReset && (
+            <OrderTableFiltersResult
+              filters={filters}
+              totalResults={dataFiltered.length}
+              onResetPage={table.onResetPage}
+              sx={{ p: 2.5, pt: 0 }}
+            />
+          )}
+
+          <Box sx={{ position: 'relative' }}>
+            <TableSelectedAction
+              dense={table.dense}
+              numSelected={table.selected.length}
+              rowCount={dataFiltered.length}
+              onSelectAllRows={(checked) =>
+                table.onSelectAllRows(
+                  checked,
+                  dataFiltered.map((row) => row.id)
+                )
+              }
+              action={
+                <Tooltip title="Delete">
+                  <IconButton color="primary" onClick={confirm.onTrue}>
+                    <Iconify icon="solar:trash-bin-trash-bold" />
+                  </IconButton>
+                </Tooltip>
+              }
+            />
+            <Dialog open={dialog.value} onClose={handleDialogClose} fullWidth maxWidth="sm">
+              <DialogTitle>{isEdit.value ? 'Edit Survey' : 'New Survey'}</DialogTitle>
+
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  fullWidth
+                  name="name"
+                  margin="dense"
+                  variant="outlined"
+                  label="Survey Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+
+                <Box sx={{ my: 2 }} />
+
+                <Autocomplete
+                  fullWidth
+                  options={ALL_CLIENTS}
+                  getOptionLabel={(option) => option.name}
+                  renderInput={(params) => <TextField {...params} label="Client" margin="none" />}
+                  renderOption={(props, option) => (
+                    <li {...props} key={option.name}>
+                      {option.name}
+                    </li>
+                  )}
+                />
+                <Box sx={{ my: 2 }} />
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <MobileDatePicker
+                      orientation="portrait"
+                      label="Start Date"
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      slotProps={{ textField: { fullWidth: true } }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <MobileDatePicker
+                      orientation="portrait"
+                      label="End Date"
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      slotProps={{ textField: { fullWidth: true } }}
+                    />
+                  </Grid>
+                </Grid>
+                <Box sx={{ my: 2 }} />
+                <Autocomplete
+                  fullWidth
+                  options={ALL_MANAGERS}
+                  getOptionLabel={(option) => option.name}
+                  renderInput={(params) => <TextField {...params} label="Manager" margin="none" />}
+                  renderOption={(props, option) => (
+                    <li {...props} key={option.name}>
+                      {option.name} - {option.department}
+                    </li>
+                  )}
+                />
+
+                <Box sx={{ my: 2 }} />
+                <TextField
+                  variant="outlined"
+                  rows={4}
+                  fullWidth
+                  multiline
+                  label="Descrption"
+                  defaultValue="Default Value"
+                />
+              </DialogContent>
+
+              <DialogActions>
+                <Button onClick={handleDialogClose} variant="outlined" color="inherit">
+                  Cancel
+                </Button>
+                <Button onClick={handleSubmit} variant="contained">
+                  {isEdit.value ? 'Update' : 'Create'}
+                </Button>
+              </DialogActions>
+            </Dialog>
+            <Scrollbar sx={{ minHeight: 444 }}>
+              <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: 960 }}>
+                <TableHeadCustom
+                  order={table.order}
+                  orderBy={table.orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={dataFiltered.length}
+                  numSelected={table.selected.length}
+                  onSort={table.onSort}
+                  onSelectAllRows={(checked) =>
+                    table.onSelectAllRows(
+                      checked,
+                      dataFiltered.map((row) => row.id)
+                    )
+                  }
+                />
+
+                <TableBody>
+                  {dataFiltered
+                    .slice(
+                      table.page * table.rowsPerPage,
+                      table.page * table.rowsPerPage + table.rowsPerPage
+                    )
+                    .map((row) => (
+                      <OrderTableRow
+                        key={row.id}
+                        row={row}
+                        selected={table.selected.includes(row.id)}
+                        onSelectRow={() => table.onSelectRow(row.id)}
+                        onEditRow={() => handleEditRow(row.id)}
+                        onDeleteRow={() => handleDeleteRow(row.id)}
+                        onViewRow={() => handleViewRow(row.id)}
+                      />
+                    ))}
+
+                  <TableEmptyRows
+                    height={table.dense ? 56 : 56 + 20}
+                    emptyRows={emptyRows(table.page, table.rowsPerPage, dataFiltered.length)}
+                  />
+
+                  <TableNoData notFound={notFound} />
+                </TableBody>
+              </Table>
+            </Scrollbar>
+          </Box>
+
+          <TablePaginationCustom
+            page={table.page}
+            dense={table.dense}
+            count={dataFiltered.length}
+            rowsPerPage={table.rowsPerPage}
+            onPageChange={table.onChangePage}
+            onChangeDense={table.onChangeDense}
+            onRowsPerPageChange={table.onChangeRowsPerPage}
+          />
+        </Card>
+      </DashboardContent>
+
+      <ConfirmDialog
+        open={confirm.value}
+        onClose={confirm.onFalse}
+        title="Delete"
+        content={
+          <>
+            Are you sure want to delete <strong> {table.selected.length} </strong> items?
+          </>
+        }
+        action={
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              handleDeleteRows();
+              confirm.onFalse();
+            }}
+          >
+            Delete
+          </Button>
+        }
+      />
+    </>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+type ApplyFilterProps = {
+  dateError: boolean;
+  inputData: IOrderItem[];
+  filters: IOrderTableFilters;
+  comparator: (a: any, b: any) => number;
+};
+
+function applyFilter({ inputData, comparator, filters, dateError }: ApplyFilterProps) {
+  const { status, name, startDate, endDate } = filters;
+
+  const stabilizedThis = inputData.map((el, index) => [el, index] as const);
+
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+
+  inputData = stabilizedThis.map((el) => el[0]);
+
+  if (name) {
+    inputData = inputData.filter(
+      (order) =>
+        order.orderNumber.toLowerCase().indexOf(name.toLowerCase()) !== -1 ||
+        order.customer.name.toLowerCase().indexOf(name.toLowerCase()) !== -1 ||
+        order.customer.email.toLowerCase().indexOf(name.toLowerCase()) !== -1
+    );
+  }
+
+  if (status !== 'all') {
+    inputData = inputData.filter((order) => order.status === status);
+  }
+
+  if (!dateError) {
+    if (startDate && endDate) {
+      inputData = inputData.filter((order) => fIsBetween(order.createdAt, startDate, endDate));
+    }
+  }
+
+  return inputData;
+}
