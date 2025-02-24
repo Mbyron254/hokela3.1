635242d251e5709e369831a6815de78ace653094
Mbyron254 <byron@hokela.co.ke>
2025-02-24 11:36:17 +0300

    Add Client Details View

---
+++ src/app/v2/marketing/dashboard/clients/[id]/page.tsx
@@ -0,0 +1,21 @@
+import axios, { endpoints } from 'src/utils/axios';
+
+import { CONFIG } from 'src/config-global';
+
+import { ClientDetailsView } from 'src/sections/clients/view/client-details-view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = { title: `Client details | Dashboard - ${CONFIG.appName}` };
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function Page({ params }: Props) {
+  const { id } = params;
+
+  console.log('id', id);
+
+  return <ClientDetailsView id={id} />;
+}
