9b1e1578a79ebeb9e1f00d689143b5352a83af6c
Mbyron254 <byron@hokela.co.ke>
2025-02-25 11:59:04 +0300

    Add Custom Loader

--- src/app/v2/marketing/dashboard/projects/[id]/loading.tsx
+++ src/app/v2/marketing/dashboard/projects/[id]/loading.tsx
@@ -1,15 +1,22 @@
-'use client';
+// 'use client';
 
-import { DashboardContent } from 'src/layouts/dashboard';
+// import { DashboardContent } from 'src/layouts/dashboard';
 
-import { ProductDetailsSkeleton } from 'src/sections/clients/client-skeleton';
+// import { ProductDetailsSkeleton } from 'src/sections/clients/client-skeleton';
+
+// // ----------------------------------------------------------------------
+
+// export default function Loading() {
+//   return (
+//     <DashboardContent sx={{ pt: 5 }}>
+//       <ProductDetailsSkeleton />
+//     </DashboardContent>
+//   );
+// }
+import { LoadingScreen } from 'src/components/loading-screen';
 
 // ----------------------------------------------------------------------
 
 export default function Loading() {
-  return (
-    <DashboardContent sx={{ pt: 5 }}>
-      <ProductDetailsSkeleton />
-    </DashboardContent>
-  );
+  return <LoadingScreen />;
 }
