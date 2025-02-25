60c996f0f8e9f60efb170778c43cc8c07bd51093
Mbyron254 <byron@hokela.co.ke>
2025-02-25 11:57:21 +0300

    Update Project Details Layout

--- src/sections/projects/view/project-details-view.tsx
+++ src/sections/projects/view/project-details-view.tsx
@@ -2,70 +2,84 @@
 
 import type { IOrderItem } from 'src/types/project';
 
-import { useState, useCallback } from 'react';
-
-import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Unstable_Grid2';
+import { useEffect, useState } from 'react';
 
 import { paths } from 'src/routes/paths';
 
-import { STATUS_OPTION } from 'src/_mock';
 import { DashboardContent } from 'src/layouts/dashboard';
 
-import { ProjectDetailsInfo } from '../project-details-info';
-import { ProjectDetailsItems } from '../project-details-item';
-import { ProjectDetailsHistory } from '../project-details-history';
-import { ProjectDetailsToolbar } from '../project-details-toolbar';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
+import { PROJECT } from 'src/lib/mutations/project.mutation';
+import { GQLMutation } from 'src/lib/client';
+import { Iconify } from 'src/components/iconify';
+import { Tab, Tabs } from '@mui/material';
+import { Paper } from '@mui/material';
+import { Typography } from '@mui/material';
+import { CampaignListView } from './campaign-list-view';
 
 // ----------------------------------------------------------------------
 
+const TABS = [
+  { value: 'overview', icon: <Iconify icon="solar:phone-bold" width={24} />, label: 'Overview' },
+  {
+    value: 'campaigns',
+    icon: <Iconify icon="solar:heart-bold" width={24} />,
+    label: 'Campaigns',
+  },
+];
+
 type Props = {
-  order?: IOrderItem;
+  id: string;
 };
 
-export function ProjectDetailsView({ order }: Props) {
-  const [status, setStatus] = useState(order?.status);
+export function ProjectDetailsView({ id }: Props) {
+  const [currentTab, setCurrentTab] = useState('overview');
 
-  const handleChangeStatus = useCallback((newValue: string) => {
-    setStatus(newValue);
-  }, []);
+  const { action: getProject, data: project } = GQLMutation({
+    mutation: PROJECT,
+    resolver: 'project',
+    toastmsg: false,
+  });
+
+  const loadProject = () => {
+    if (id) getProject({ variables: { input: { id } } });
+  };
+  // const { product } = await getProduct(id);
+  useEffect(() => loadProject(), []);
+
+  console.log('project', project);
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setCurrentTab(newValue);
+  };
 
   return (
     <DashboardContent>
-      <ProjectDetailsToolbar
-        backLink={paths.v2.marketing.root}
-        orderNumber={order?.orderNumber}
-        createdAt={order?.createdAt}
-        status={status}
-        onChangeStatus={handleChangeStatus}
-        statusOptions={STATUS_OPTION}
+      <CustomBreadcrumbs
+        heading="Project Details"
+        links={[
+          { name: 'Dashboard', href: paths.v2.marketing.root },
+          { name: 'Project', href: paths.v2.marketing.products.overview },
+          { name: 'Details' },
+        ]}
+        sx={{ mb: { xs: 3, md: 5 } }}
       />
-
-      <Grid container spacing={3}>
-        <Grid xs={12} md={8}>
-          <Stack spacing={3} direction={{ xs: 'column-reverse', md: 'column' }}>
-            <ProjectDetailsItems
-              items={order?.items}
-              taxes={order?.taxes}
-              shipping={order?.shipping}
-              discount={order?.discount}
-              subtotal={order?.subtotal}
-              totalAmount={order?.totalAmount}
-            />
-
-            <ProjectDetailsHistory history={order?.history} />
-          </Stack>
-        </Grid>
-
-        <Grid xs={12} md={4}>
-          <ProjectDetailsInfo
-            customer={order?.customer}
-            delivery={order?.delivery}
-            payment={order?.payment}
-            shippingAddress={order?.shippingAddress}
-          />
-        </Grid>
-      </Grid>
+      <Tabs value={currentTab} onChange={handleTabChange}>
+        {TABS.map((tab) => (
+          <Tab key={tab.value} value={tab.value} label={tab.label} />
+        ))}
+      </Tabs>
+      <Paper variant="outlined" sx={{ p: 2.5, typography: 'body2', borderRadius: 1.5 }}>
+        {currentTab === 'overview' && (
+          <>
+            <Typography variant="h6" gutterBottom>
+              {project?.name}
+            </Typography>
+            Overview Content
+          </>
+        )}
+        {currentTab === 'campaigns' && <CampaignListView id={id} />}
+      </Paper>
     </DashboardContent>
   );
 }
