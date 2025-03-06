3d71d7985a40424eff8c1142418c52cddc268027
Mbyron254 <byron@hokela.co.ke>
2025-03-06 10:08:55 +0300

    Add Enterprise Clients Details Page

---
+++ src/app/v2/admin/dashboard/clients/enterprise/[id]/page.tsx
@@ -0,0 +1,7 @@
+import EnterpriseDetailsView from 'src/sections/admin/clients/enterprise/details/view/enterprise-details-view';
+
+export const metadata = { title: `Enterprise Details` };
+
+export default function Page() {
+  return <EnterpriseDetailsView />;
+}
