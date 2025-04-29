86ae0ec6ea4cf8643332231783c0471671e40716
WGMI <imo.muganda21@gmail.com>
2025-04-29 10:55:59 +0300

    Test change (Remove later)

--- src/sections/admin/clients/enterprise/view/enterprise-list-view.tsx
+++ src/sections/admin/clients/enterprise/view/enterprise-list-view.tsx
@@ -1,127 +1,14 @@
-'use client';
-
-import { useEffect, useMemo, useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import {
-  Box,
-  Card,
-  Button,
-  Dialog,
-  TextField,
-  DialogTitle,
-  Autocomplete,
-  DialogContent,
-  DialogActions,
-} from '@mui/material';
-
-import { paths } from 'src/routes/paths';
-
-import { useBoolean } from 'src/hooks/use-boolean';
-
-import { _mock } from 'src/_mock';
-import { GQLQuery, GQLMutation } from 'src/lib/client';
-import { DashboardContent } from 'src/layouts/dashboard';
+import React, { useState } from 'react';
+import { TextField, Button, Dialog, DialogActions, DialogContent, DialogTitle, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import { GQLMutation, GQLQuery } from 'src/lib/client';
+import { CLIENT_T1_CREATE } from 'src/lib/mutations/client-t1.mutation';
 import { Q_CLIENT_TYPES_MINI } from 'src/lib/queries/client.query';
-import {
-  CLIENT_T1_CREATE,
-  CLIENT_T1_UPDATE,
-  CLIENT_T1_RECYCLE,
-  CLIENT_T1_RESTORE,
-  M_CLIENT_T1
-} from 'src/lib/mutations/client-t1.mutation';
-// import { Q_CLIENTS_T1 } from 'src/lib/queries/client-t1.query';
 
-import { Iconify } from 'src/components/iconify';
-import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
-
-import { DataGridCustom } from '../data-grid-custom';
-
-[...Array(20)].map((_, index) => {
-  const status =
-    (index % 2 && 'online') || (index % 3 && 'alway') || (index % 4 && 'busy') || 'offline';
-
-  return {
-    id: _mock.id(index),
-    status,
-    email: _mock.email(index),
-    name: _mock.fullName(index),
-    age: _mock.number.age(index),
-    lastLogin: _mock.time(index),
-    isAdmin: _mock.boolean(index),
-    lastName: _mock.lastName(index),
-    rating: _mock.number.rating(index),
-    firstName: _mock.firstName(index),
-    performance: _mock.number.percent(index),
-  };
-});
-
-// If you need to log the array, do it after it's been created
-// console.log('dataGrid', _dataGrid);
-
-type IClient = {
-  id: string;
-  clientNo: string;
-  name: string;
-  clientType: string;
-  createdAt: string;
-  status?: string;
-  // lastLogin: IDateValue;
-};
-type TClientType = {
-  __typename?: 'TClientType';
-  id: string;
-  name: string;
-};
-type ClientType = {
-  id: string;
-  name: string;
-};
-
-type TClientTier1 = {
-  __typename: 'TClientTier1';
-  index: number;
-  id: string;
-  name: string;
-  clientNo: string;
-  created: string;
-  clientType: TClientType;
-};
-
-type TClientsTier1 = {
-  __typename: 'TClientsTier1';
-  count: number;
-  rows: TClientTier1[];
-};
-
-export default function EnterpriseListView() {
-  const router = useRouter();
-  const [tableData, setTableData] = useState<IClient[]>([]);
-
-  const usersQueryFilters = useMemo(() => ({ page: 0, pageSize: 10 }), []);
-
-  const [client_types, setClientsType] = useState<ClientType[]>([]);
-
-  // ----------------------------- -------------------------------------------------
-  const { data: clientTypes } = GQLQuery({
-    query: Q_CLIENT_TYPES_MINI,
-    queryAction: 'clientTypes',
-    variables: { input: {} },
-  });
-  // const {
-  //   refetch: refetchClientsActive,
-  //   data: clientsActive,
-  //   loading: loadingCientsActive,
-  // } = GQLQuery({
-  //   query: Q_CLIENTS_T1,
-  //   queryAction: 'tier1Clients',
-  //   variables: { input: usersQueryFilters },
-  // });
-
-  const { action: fetchClientsActive } = GQLMutation({
-    mutation: M_CLIENT_T1,
-    resolver: 'fetchClientsActive',
-    toastmsg: true,
+const EnterpriseListView = () => {
+  const [open, setOpen] = useState(false);
+  const [inputCreate, setInputCreate] = useState({
+    clientTypeId: '',
+    name: '',
   });
 
   const { action: create, loading: creating } = GQLMutation({
@@ -130,251 +17,70 @@ export default function EnterpriseListView() {
     toastmsg: true,
   });
 
-  const { action: update, loading: updating } = GQLMutation({
-    mutation: CLIENT_T1_UPDATE,
-    resolver: 'tier1ClientUpdate',
-    toastmsg: true,
-  });
-  const { action: recycle, loading: recycling } = GQLMutation({
-    mutation: CLIENT_T1_RECYCLE,
-    resolver: 'tier1ClientRecycle',
-    toastmsg: true,
-  });
-  const { action: restore, loading: restoring } = GQLMutation({
-    mutation: CLIENT_T1_RESTORE,
-    resolver: 'tier1ClientRestore',
-    toastmsg: true,
+  const { data: clientTypes } = GQLQuery({
+    query: Q_CLIENT_TYPES_MINI,
+    queryAction: 'clientTypes',
+    variables: { input: {} },
   });
 
-  const [selectedActive, setSelectedActive] = useState<string[]>([]);
-  const [selectedRecycled, setSelectedRecycled] = useState<string[]>([]);
-
-  // useMemo(() => {
-  //   console.log('Memo starts');
-  //   const clientsTypeData = clientTypes?.rows ? transformClientTypesData(clientTypes.rows) : [];
-  //   console.log('clientsType:', clientsTypeData);
-  //   if (clientsActive) {
-  //     const activeClients = transformClientData(clientsActive.rows);
-  //     // const recycledClients = transformRecycledClientData(clientsRecycled.rows);
-  //     const clientsType = clientTypes?.rows ? transformClientTypesData(clientTypes.rows) : [];
-  //     setClientsType(clientsType);
-
-  //     setTableData([...activeClients]);
-  //   }
-  // }, [clientsActive, clientTypes]);
-
-  useEffect(() => {
-    fetchClientsActive({ variables: { input: usersQueryFilters } });
-  }, [fetchClientsActive, usersQueryFilters]);
-
-  useEffect(() => {
-    console.log('fetchClientsActive before', fetchClientsActive);
-    if (fetchClientsActive) {
-      console.log('fetchClientsActive after', fetchClientsActive);
-      const activeClients = transformClientData(fetchClientsActive.rows);
-      setTableData(activeClients);
-    }
-  }, [fetchClientsActive]);
-
-  useEffect(() => {
-    if (clientTypes) {
-      const clientsType = clientTypes.rows;
-      console.log('client types:', clientTypes);
-    }
-  }, [clientTypes]);
-
-  // -----------------------------enterprise form -------------------------------------------------
-  const isEdit = useBoolean();
-  const dialog = useBoolean();
-
-  const [formData, setFormData] = useState({
-    id: '',
-    name: '',
-    client_type: '',
-  });
-
-  const handleNewRow = () => {
-    dialog.onTrue();
-  };
-  const handleEditRow = (row: any) => {
-    if (row) {
-      console.log('Editing row with ID:', row);
-
-      console.log(tableData, 'Client');
-    }
+  const handleCreate = () => {
+    create({ variables: { input: inputCreate } });
+    setOpen(false);
   };
 
-  const handleViewRow = (row: any) => {
-    if (row) {
-      router.replace(paths.v2.admin.clients.enterprise.details(row?.id));
-    }
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handleDialogClose = () => {
-    setFormData({
-      id: '',
-      name: '',
-      client_type: '',
-    });
-    dialog.onFalse();
+  const handleClose = () => {
+    setOpen(false);
   };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleClientTypeChange = (_: any, value: any) => {
-    setFormData({
-      ...formData,
-      client_type: value?.id || '',
-    });
-  };
-  const handleSubmit = () => {
-    if (isEdit.value) {
-      console.log('Edit');
-    } else {
-      console.log(formData, 'formData');
-      const inputCreate = {
-        clientTypeId: formData.client_type,
-        name: formData.name,
-      };
-      create({ variables: { input: inputCreate } });
-    }
-    handleDialogClose();
-  };
-
-  const handleRecycle = () => {
-    if (selectedActive.length) {
-      recycle({ variables: { input: { ids: selectedActive } } });
-    }
-  };
-
-  const handleRestore = () => {
-    if (selectedRecycled.length) {
-      restore({ variables: { input: { ids: selectedRecycled } } });
-    }
-  };
-
-  const handleUpdate = () => {
-    if (isEdit.value) {
-      const inputUpdate = {
-        id: formData.id,
-        clientTypeId: formData.client_type,
-        name: formData.name,
-      };
-      update({ variables: { input: inputUpdate } });
-    }
-    handleDialogClose();
-  };
-
-  // ---------------------------enterprise form ----------------------------------------------------
 
   return (
-    <DashboardContent>
-      <Card sx={{ p: 5 }}>
-        <Box sx={{ height: 600 }}>
-          <CustomBreadcrumbs
-            heading="Enterprise Clients"
-            links={[
-              { name: 'Dashboard', href: paths.v2.admin.root },
-              { name: 'Enterprise Clients', href: paths.v2.admin.clients.enterprise.root },
-              { name: 'Enterprise Clients List' },
-            ]}
-            action={
-              <Button
-                onClick={() => {
-                  handleNewRow();
-                  isEdit.onFalse();
-                }}
-                variant="contained"
-                startIcon={<Iconify icon="mingcute:add-line" />}
-              >
-                New Client
-              </Button>
-            }
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add New Client
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New Client</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Client Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={inputCreate.name}
+            onChange={(e) => setInputCreate({ ...inputCreate, name: e.target.value })}
           />
-          <Dialog open={dialog.value} onClose={handleDialogClose} fullWidth maxWidth="sm">
-            <DialogTitle>
-              {isEdit.value ? 'Edit Enterprise Client' : 'New Enterprise Client'}
-            </DialogTitle>
-
-            <DialogContent>
-              <TextField
-                autoFocus
-                fullWidth
-                name="name"
-                margin="dense"
-                variant="outlined"
-                label="Client Name"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-              <Box sx={{ my: 2 }} />
-              <Autocomplete
-                fullWidth
-                options={client_types ?? []}
-                value={
-                  client_types?.find(
-                    (client: { id: string }) => client.id === formData.client_type
-                  ) || null
-                }
-                getOptionLabel={(option) => option.name}
-                onChange={handleClientTypeChange}
-                renderInput={(params) => (
-                  <TextField {...params} label="Clients Type" margin="none" />
-                )}
-                renderOption={(props, option) => (
-                  <li {...props} key={option.id}>
-                    {option.name}
-                  </li>
-                )}
-              />
-            </DialogContent>
-
-            <DialogActions>
-              <Button onClick={handleDialogClose} variant="outlined" color="inherit">
-                Cancel
-              </Button>
-              <Button onClick={isEdit.value ? handleUpdate : handleSubmit} variant="contained">
-                {isEdit.value ? 'Update' : 'Create'}
-              </Button>
-            </DialogActions>
-          </Dialog>
-          <DataGridCustom
-            data={tableData ?? []}
-            handleEditRow={handleEditRow}
-            handleViewRow={handleViewRow}
-          />
-        </Box>
-      </Card>
-    </DashboardContent>
+          <FormControl fullWidth variant="standard" margin="dense">
+            <InputLabel id="client-type-label">Client Type</InputLabel>
+            <Select
+              labelId="client-type-label"
+              id="clientType"
+              value={inputCreate.clientTypeId}
+              onChange={(e) => setInputCreate({ ...inputCreate, clientTypeId: e.target.value })}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {clientTypes?.rows.map((clientType: any, index: number) => (
+                <MenuItem value={clientType.id} key={`client-type-${index}`}>
+                  {clientType.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleCreate} disabled={creating}>Create</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
-}
+};
 
-const transformClientData = (clients: Array<TClientTier1>) =>
-  clients.map((client) => ({
-    id: client.id,
-    clientNo: client.clientNo, // Add clientNo field
-    name: client.name,
-    clientType: client.clientType.name,
-    createdAt: client.created,
-    status: 'active', // Add status field
-  }));
-
-// Transform clientsRecycled data
-const transformRecycledClientData = (clients: Array<TClientTier1>) =>
-  clients.map((client) => ({
-    id: client.id,
-    clientNo: client.clientNo, // Add clientNo field
-    name: client.name,
-    clientType: client.clientType.name,
-    createdAt: client.created,
-    status: 'suspended', // Add status field
-  }));
-const transformClientTypesData = (clientTypes: Array<TClientType>) =>
-  clientTypes.map((clientType) => ({
-    id: clientType.id,
-    name: clientType.name,
-  }));
+export default EnterpriseListView;
