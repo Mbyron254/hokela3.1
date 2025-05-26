0617787a80bc23521d0cdfcbb0de49100e9cbc8d
WGMI <imo.muganda21@gmail.com>
2025-05-26 10:11:19 +0300

    Server.js update

--- server.js
+++ server.js
@@ -69,6 +69,92 @@ const proxyFD = createProxyMiddleware({
   },
 });
 
+const proxyCSVDSurveyReports = createProxyMiddleware({
+  logLevel: 'warn',
+  target: dev ? `${SCHEME}://${HOST_SERVER}:3000` : `${SCHEME}://${HOST_SERVER}`,
+  changeOrigin: true,
+  ignorePath: false,
+  pathRewrite: (path, req) => {
+    return '/excel/survey-reports';
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (dev) {
+      proxyReq.setHeader(HEADER_KEY_CLIENT, HEADER_VAL_CLIENT);
+    }
+  },
+  onError: (proxyError, req, res, target) => {
+    console.log('::::::: Proxy error CSV DSurvey Reports: ', proxyError);
+  },
+});
+const proxyCSVDAgents = createProxyMiddleware({
+  logLevel: 'warn',
+  target: dev ? `${SCHEME}://${HOST_SERVER}:3000` : `${SCHEME}://${HOST_SERVER}`,
+  changeOrigin: true,
+  ignorePath: false,
+  pathRewrite: (path, req) => {
+    return '/excel/agents';
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (dev) {
+      proxyReq.setHeader(HEADER_KEY_CLIENT, HEADER_VAL_CLIENT);
+    }
+  },
+  onError: (proxyError, req, res, target) => {
+    console.log('::::::: Proxy error CSVD Agents: ', proxyError);
+  },
+});
+const proxyCSVDAgentsRunOffers = createProxyMiddleware({
+  logLevel: 'warn',
+  target: dev ? `${SCHEME}://${HOST_SERVER}:3000` : `${SCHEME}://${HOST_SERVER}`,
+  changeOrigin: true,
+  ignorePath: false,
+  pathRewrite: (path, req) => {
+    return '/excel/agents-run-offers';
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (dev) {
+      proxyReq.setHeader(HEADER_KEY_CLIENT, HEADER_VAL_CLIENT);
+    }
+  },
+  onError: (proxyError, req, res, target) => {
+    console.log('::::::: Proxy error CSVD Agents Run Offers: ', proxyError);
+  },
+});
+const proxyCSVDAgentsRunSalesCumulative = createProxyMiddleware({
+  logLevel: 'warn',
+  target: dev ? `${SCHEME}://${HOST_SERVER}:3000` : `${SCHEME}://${HOST_SERVER}`,
+  changeOrigin: true,
+  ignorePath: false,
+  pathRewrite: (path, req) => {
+    return '/excel/agents-run-sales-cumulative';
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (dev) {
+      proxyReq.setHeader(HEADER_KEY_CLIENT, HEADER_VAL_CLIENT);
+    }
+  },
+  onError: (proxyError, req, res, target) => {
+    console.log('::::::: Proxy error CSVD Agents Run Sales Cumulative: ', proxyError);
+  },
+});
+const proxyCSVDAgentsRunSalesDaily = createProxyMiddleware({
+  logLevel: 'warn',
+  target: dev ? `${SCHEME}://${HOST_SERVER}:3000` : `${SCHEME}://${HOST_SERVER}`,
+  changeOrigin: true,
+  ignorePath: false,
+  pathRewrite: (path, req) => {
+    return '/excel/agents-run-sales-daily';
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (dev) {
+      proxyReq.setHeader(HEADER_KEY_CLIENT, HEADER_VAL_CLIENT);
+    }
+  },
+  onError: (proxyError, req, res, target) => {
+    console.log('::::::: Proxy error CSVD Agents Run Sales Daily: ', proxyError);
+  },
+});
+
 app.prepare().then(() => {
   createServer((req, res) => {
     try {
@@ -81,11 +167,27 @@ app.prepare().then(() => {
         const path_parts = pathname.split('/');
         const isFR = path_parts[1] === 'document' && path_parts[2] === 'f';
         const isFD = path_parts[1] === 'document' && path_parts[2] === 'd';
+        const isCSVD_SR = path_parts[1] === 'excel' && path_parts[2] === 'survey-reports';
+        const isCSVD_A = path_parts[1] === 'excel' && path_parts[2] === 'agents';
+        const isCSVD_ARO = path_parts[1] === 'excel' && path_parts[2] === 'agents-run-offers';
+        const isCSVD_ARSC = path_parts[1] === 'excel' && path_parts[2] === 'agents-run-sales-cumulative';
+        const isCSVD_ARSD = path_parts[1] === 'excel' && path_parts[2] === 'agents-run-sales-daily';
+
 
         if (isFR) {
           return proxyFR(req, res);
         } else if (isFD) {
           return proxyFD(req, res);
+        } else if (isCSVD_SR) {
+          return proxyCSVDSurveyReports(req, res);
+        } else if (isCSVD_A) {
+          return proxyCSVDAgents(req, res);
+        } else if (isCSVD_ARO) {
+          return proxyCSVDAgentsRunOffers(req, res);
+        } else if (isCSVD_ARSC) {
+          return proxyCSVDAgentsRunSalesCumulative(req, res);
+        } else if (isCSVD_ARSD) {
+          return proxyCSVDAgentsRunSalesDaily(req, res);
         } else {
           nextHandler(req, res, parsedUrl);
         }
