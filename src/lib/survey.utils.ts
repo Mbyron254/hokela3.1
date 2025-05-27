a1adef79fdbce4d9c060a716a650bca0a9dba454
WGMI <imo.muganda21@gmail.com>
2025-05-27 10:14:25 +0300

    Middleware update

--- src/lib/survey.utils.ts
+++ src/lib/survey.utils.ts
@@ -20,7 +20,7 @@ export const formElementAdd = (
   };
 
   const updatedFields = [...questionnaireFields, formElement];
-  console.log(updatedFields);
+  console.log('updatedFields: ',updatedFields);
   setQuestionnaireFields(updatedFields);
 };
 
